f93638fbf9bf408415f0d140ef80d1516b4d7a1f
Daniel <danielfelipe2905@gmail.com>
2023-08-26 22:29:48 -0300

    Novas adições

--- string_invertido_2.js
+++ string_invertido_2.js
@@ -1,6 +1,20 @@
 //Faça um programa que peça ao usuário que digite uma string e imprima a string ao contrário.
 
 function reverseString(str) {
-  return str
+  // Passo 1. Use o método split() para retornar um novo array
+  var splitString = str.split("") // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Passo 2. Use o método reverse() para inverter o array recém-criado
+  var reverseArray = splitString.reverse() // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Passo 3. Use o método join() para unir todos os elementos do array em uma string
+  var joinArray = reverseArray.join("") // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+
+  // Passo 4. Retorne a string invertida
+  return joinArray // "olleh"
 }
+
 reverseString("hello")
